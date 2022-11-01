207c2dce19d532f40f9ce428db7e4fd34bacddfd
zsderw <oood0903@gmail.com>
2022-11-01 22:07:57 +0900

    .

--- lab5/q4.py
+++ lab5/q4.py
@@ -8,4 +8,8 @@ with open(fname, "rt") as fp:
     for line in fp:
         if line.startswith("From: ") :
             str_arr = line.split()
-            if str_arr[1]
+            if str_arr[1] not in from_dict:
+                from_dict[str_arr[1]] = 1
+            else:
+                from_dict[str_arr[1]] += 1
+print(from_dict)
